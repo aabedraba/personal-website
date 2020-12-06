f697cac0375151d8bb681e885e9fcc4c6541e7ac
Abdallah <aabedraba@gmail.com>
2020-12-06 15:52:44 +0100

    Add esm and get next server through express

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,21 @@
 import express from "express";
+import next from "next";
+import { parse } from "url";
 
 const server = express();
 
-server.get("/", (req, res) => {
-  res.send("Hello");
-});
+const dev = process.env.NODE_ENV !== "production";
+const nextApp = next({ dev });
+const nextHanlder = nextApp.getRequestHandler();
 
-const PORT = process.env.PORT || 8000;
-server.listen(PORT, () => {
-  console.log("Listening on http://localhost:" + PORT);
+nextApp.prepare().then(() => {
+  server.use((req, res) => {
+    const parsedUrl = parse(req.url, true);
+    nextHanlder(req, res, parsedUrl);
+  });
+
+  const PORT = process.env.PORT || 8000;
+  server.listen(PORT, () => {
+    console.log("Listening on http://localhost:" + PORT);
+  });
 });
